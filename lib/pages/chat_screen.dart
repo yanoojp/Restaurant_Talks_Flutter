ce03e95a8912e115423feea26aa55dfdb4b5513f
yanoojp <120234755+yanoojp@users.noreply.github.com>
2022-12-29 18:23:33 +0900

    チャットに画像送信機能を追加

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:restaurant_talks_flutter/fixedDatas/variables.dart';
 
 import '../components/header.dart';
@@ -30,6 +31,30 @@ class ChatScreenState extends State<ChatScreen> {
   final List<types.Message> _messages = [];
   final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
 
+  void _handleImageSelection() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedImage != null) {
+      final bytes = await pickedImage.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: randomString(),
+        name: pickedImage.name,
+        size: bytes.length,
+        uri: pickedImage.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: Header(
@@ -40,6 +65,7 @@ class ChatScreenState extends State<ChatScreen> {
     body: Chat(
       user: _user,
       messages: _messages,
+      onAttachmentPressed: _handleImageSelection,
       onSendPressed: _handleSendPressed,
     ),
     bottomNavigationBar: BottomNavigation(
