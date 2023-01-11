3974d2d178853c87efae600f3d018397ea03afae
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-01-11 15:21:14 +0900

    カテゴリーで絞り込み機能の実装

--- lib/utils/firestore/items.dart
+++ lib/utils/firestore/items.dart
@@ -27,4 +27,20 @@ class ItemFirestore {
   static Future<dynamic> deleteItem(String docId) async {
     await items.doc(docId).delete();
   }
+
+  static Stream<QuerySnapshot<Object?>>? getItem(accountId, selectedCategoryValue) {
+    try {
+      if (selectedCategoryValue == sortByCategoryLabel) {
+        return items.where('accountId', isEqualTo: accountId).orderBy('updatedAt').snapshots();
+      } else if (selectedCategoryValue == appetizerLabel) {
+        return items.where('accountId', isEqualTo: accountId).where('category', isEqualTo: appetizerLabel).orderBy('updatedAt').snapshots();
+      } else if (selectedCategoryValue == mainDishLabel) {
+        return items.where('accountId', isEqualTo: accountId).where('category', isEqualTo: mainDishLabel).orderBy('updatedAt').snapshots();
+      } else if (selectedCategoryValue == beverageLabel) {
+        return items.where('accountId', isEqualTo: accountId).where('category', isEqualTo: beverageLabel).orderBy('updatedAt').snapshots();
+      }
+    } on FirebaseException catch(e) {
+      return null;
+    }
+  }
 }
